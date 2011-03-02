e5e643547a4f33682c2213c755a9ce75f49df867
Stu Rob <stumrob@gmail.com>
2011-03-02 12:39:18 +0000

    favicons updating mostly

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ var app = $.sammy(function() {
     dates.start = Date.parse( this.params['date'] );
     dates.end = Date.parse( this.params['date'] );
 
-    var days = parseInt(this.params['days'], 10);
+    var days = parseInt( this.params['days'], 10 );
     
     dates.end.add( days ).days();
     
@@ -30,8 +30,18 @@ var app = $.sammy(function() {
     var done = dates.today - dates.start;
     var percent = (done / length) * 100;
     
-    var days_left = Math.ceil((dates.end - dates.today) / DAY);
-    var days_to_start = Math.ceil((dates.start - dates.today) / DAY);
+    var days_left = Math.floor( (dates.end - dates.today) / DAY );
+    var days_to_start = Math.ceil( (dates.start - dates.today) / DAY );
+    
+    
+    
+    var hex_section = Math.hem( Math.ceil( (percent / 100) * 16 ), 0, 16 );
+    
+    // $('link[rel*=shortcut]').attr( 'href', 'images/fav/fav' + hex_section + '.png' )
+                            // .remove().clone().appendTo('head');
+    
+    $('link[rel*=shortcut]').remove();
+    $('<link id="favicon" rel="shortcut icon" href="images/fav/fav' + hex_section + '.png">').appendTo('head');
     
     setTimeout(function() {
       if (dates.today > dates.end) {
@@ -59,10 +69,12 @@ var app = $.sammy(function() {
 
 
 $(function(){
-
-  
   app.run();
 });
+
+Math.hem = function (n, low, high) {
+  return Math.min(high, Math.max(low, n));
+}
 
 function s (n) {
   return n != 1 ? 's' : '';
